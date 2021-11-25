e99dfab3c1e3178c12f8d56870f525ccb4dd0c47
william <wltowxyz@gmail.com>
2021-11-25 14:51:37 -0500

    chore: refactor structure

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,1 @@
-const { baseConfig } = require('./lib/configs');
-
-module.exports = {
-  parserOptions : {
-    sourceType   : 'module',
-    ecmaFeatures : { jsx : true }
-  },
-  env : {
-    browser : true,
-    node    : true,
-    es6     : true
-  },
-  settings : baseConfig.settings,
-  plugins  : baseConfig.plugins,
-  extends  : baseConfig.extends,
-  rules    : baseConfig.rules
-};
+module.exports = { extends : ['@reflektor/react'] };
--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const { baseConfig, nextConfig } = require('./lib/configs');
-const eslint = require('./.eslintrc.js');
+const eslintrc = require('./.eslintrc.js');
+
+const baseConfig = { extends : ['@reflektor/react'] };
+const nextConfig = {
+  extends : [
+    ...baseConfig.extends,
+    'next/core-web-vitals'
+  ]
+};
 
 module.exports = {
-  rules   : eslint,
+  extends : eslintrc.extends,
   configs : {
     base : baseConfig,
     next : nextConfig
