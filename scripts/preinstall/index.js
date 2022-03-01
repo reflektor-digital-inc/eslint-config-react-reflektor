5ed1ced78d3f1a678aecb2785ed72a0a7a304bb1
wlto <william@reflektor.digital>
2022-02-28 22:56:50 -0500

    fix: remove preinstall script

--- scripts/preinstall/index.js
+++
@@ -1,9 +0,0 @@
-const pleaseUseYarn = () => {
-  const isUsingYarn = process.env.npm_config_user_agent.includes('yarn');
-  if (!isUsingYarn) {
-    console.info('\n🧶 Please use yarn for this project.\n');
-    process.exit(1);
-  }
-};
-
-pleaseUseYarn();
